6d52200c081c1a8a0180b1b60d74250d792540ae
Girish Modiletappa <girishmodiletappa@Girishs-MacBook-Air.local>
2017-08-08 08:41:07 +0530

    added comments

--- day4/cs1713-day4-prog7.c
+++ day4/cs1713-day4-prog7.c
@@ -1,3 +1,13 @@
+/*------------------------------------------------------------------------------------------------------------------
+ Name: Girish M
+ Roll number: cs1713
+ Date: 1 August 2017
+ Program description: Suppose that we choose to represent sets by arrays of non-negative integers, containing at most 100 elements each. Let A and B be 2 such sets of integers. Implement the following set operations:
+ (a) A∪B; 
+ (b) A∩B;
+ (c) A ⊆ B (returns 1 if A is a subset of B, 0 otherwise); (d) prints the power set of A on the screen.
+ Acknowledgements:
+ ------------------------------------------------------------------------------------------------------------------*/
 #include "common.h"
 
 int main(int argc, char* argv[])
